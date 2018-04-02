b5eaf17d8776e10a1cfc3e81bc1de434434d581f
gittp92 <35101854+gittp92@users.noreply.github.com>
2018-04-02 12:21:33 +0900

    feat: edit model folder to include Bb_StoreDto(Bb_Store Dto추가함) -gittp92

--- src/bba/com/a/controller/BbaStoreController.java
+++ src/bba/com/a/controller/BbaStoreController.java
@@ -6,6 +6,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class BbaStoreController {
@@ -17,7 +18,48 @@ public class BbaStoreController {
 	@RequestMapping(value="storelist.do", method=RequestMethod.GET)
 	public String storelist(Model model) {
 		logger.info("BbaStoreController storelist.do");
+		
+		
+		
+		
+		//위에 현재 위치 나타내줌 
+		model.addAttribute("doc_title", "스토어관리");
+		model.addAttribute("doc_menu", "스토어관리");
+		return "storelist.tiles";
+	}
+
+	
+	/*--------------------------------------------------------------------------------------------
+	 * store 새로 등록 
+	 *-------------------------------------------------------------------------------------------*/
+	@ResponseBody
+	@RequestMapping(value="registerStore.do", method=RequestMethod.POST)
+	public String registerStore(Model model) {
+		logger.info("BbaStoreController registerStore");
+		
+		
+		
+		
+		//위에 현재 위치 나타내줌 
+		model.addAttribute("doc_title", "스토어관리");
+		model.addAttribute("doc_menu", "스토어관리");
 		return "storelist.tiles";
 	}
 	
+	/*--------------------------------------------------------------------------------------------
+	 * store 수정
+	 *-------------------------------------------------------------------------------------------*/
+	@ResponseBody
+	@RequestMapping(value="updateStore.do", method=RequestMethod.POST)
+	public String updateStore(Model model) {
+		logger.info("BbaStoreController registerStore");
+		
+		
+		
+		
+		//위에 현재 위치 나타내줌 
+		model.addAttribute("doc_title", "스토어관리");
+		model.addAttribute("doc_menu", "스토어관리");
+		return "storelist.tiles";
+	}
 }
